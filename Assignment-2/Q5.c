e73441a6c11a9149c3d7a1b2556b7eeba2158b10
KRISHNA KUMAR GUPTA <61408269+krishnaa6268@users.noreply.github.com>
2023-10-15 13:57:55 +0530

    Update Q5.c
    The code did not fulfill some condition that is really assigned to the problem.

--- Assignment-2/Q5.c
+++ Assignment-2/Q5.c
@@ -1,14 +1,21 @@
-//5. Write a program to input a three digit number and display the sum of the digits.
+//5. Write a program to input a three-digit number and display the sum of the digits.
 
 #include<stdio.h>
 
 int main()
 {
-  int a,b,c;
-  printf("Enter Three numbers.:\n");
-  scanf("%d %d %d",&a,&b,&c);
+  int x;
+  printf("Enter a three digit number:\n");
+  scanf("%d",&x);
 
-  printf("Sum of Three Digit: %d",(a+b+c));
+  int u1,u2,u3;
+
+    u1=(x%10);
+    u2=(x/10%10);
+    u3=(x/10/10);
+
+  printf("Sum of Three Digit: %d",(u1+u2+u3));
   getch();
+
     return 0;
 }
